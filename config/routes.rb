b0ffe25ee8d4422cf43dbe7633b70bea52445f40
Julian Cesar <juliancesar@gmail.com>
2025-05-08 16:41:22 -0300

    feat: Add last_activity and last_activities endpoints to ConversationsController with corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -135,7 +135,12 @@ Rails.application.routes.draw do
               delete :avatar
             end
             scope module: :contacts do
-              resources :conversations, only: [:index]
+              resources :conversations, only: [:index] do 
+                collection do
+                  get :last_activity
+                  get :last_activities
+                end
+              end
               resources :contact_inboxes, only: [:create]
               resources :labels, only: [:create, :index]
               resources :notes
